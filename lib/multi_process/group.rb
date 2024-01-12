99c08d9cf5cc7484122916f10ff93b47ee747b04
Tyler Hunt <tyler@tylerhunt.com>
2024-01-12 09:09:13 -0500

    Ensure `Timeout` is loaded

--- lib/multi_process/group.rb
+++ lib/multi_process/group.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'timeout'
+
 module MultiProcess
   #
   # Store and run a group of processes.
--- lib/multi_process/process.rb
+++ lib/multi_process/process.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'forwardable'
+require 'timeout'
 
 module MultiProcess
   #
